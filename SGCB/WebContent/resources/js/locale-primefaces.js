fa5a18ede766ee521c1312359c734c887d6cf147
brunowmuniz <brunomuniz@outlook.com>
2015-05-21 20:24:25 -0300

    - Ajuste no mês de "março" para "marco" dentro do script usando pelo calendar. - Ajuste de quantidade de objetos dentro do ehcache.xml - Reduzido o "minQueryLenght" do busca de produtos dentro da tela de cadastrooc.xhtml

--- SGCB/WebContent/resources/js/locale-primefaces.js
+++ SGCB/WebContent/resources/js/locale-primefaces.js
@@ -3,7 +3,7 @@ PrimeFaces.locales['pt'] = {
     prevText: 'Anterior',
     nextText: 'Próximo',
     currentText: 'Começo',
-    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+    monthNames: ['Janeiro','Fevereiro','Marco','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
     monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun', 'Jul','Ago','Set','Out','Nov','Dez'],
     dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
     dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb'],
